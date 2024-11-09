15eb003301f76274cf9cf98a6028e66c5b50f7e0
Vitorgooto <101600322+Vitorgooto@users.noreply.github.com>
2024-11-09 01:53:00 -0300

    Update fishing_item.dart

--- mobile/lib/models/fishing_item.dart
+++ mobile/lib/models/fishing_item.dart
@@ -1,23 +1,43 @@
 class FishingItem {
   final int? id;
-  final String name;
-  final double weight;
+  final String nome;
+  final int peso;
+  final int pesoIsca;
+  final String material;
+  final String tipo;
+  final String ambiente;
 
-  FishingItem({this.id, required this.name, required this.weight});
+  FishingItem({
+    this.id,
+    required this.nome,
+    required this.peso,
+    required this.pesoIsca,
+    required this.material,
+    required this.tipo,
+    required this.ambiente,
+  });
 
   factory FishingItem.fromJson(Map<String, dynamic> json) {
     return FishingItem(
-      id: json['id'] is int ? json['id'] : int.tryParse(json['id'].toString()),
-      name: json['name'],
-      weight: (json['weight'] as num).toDouble(),  // Garante que o peso seja tratado como double
+      id: json['id'],
+      nome: json['nome'],
+      peso: json['peso'],
+      pesoIsca: json['peso_isca'],
+      material: json['material'],
+      tipo: json['tipo'],
+      ambiente: json['ambiente'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'name': name,
-      'weight': weight,
+      'nome': nome,
+      'peso': peso,
+      'peso_isca': pesoIsca,
+      'material': material,
+      'tipo': tipo,
+      'ambiente': ambiente,
     };
   }
 }
