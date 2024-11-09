c3664525dbcd0ac0990dc6d7a7a0ab3f5def2139
Vitorgooto <101600322+Vitorgooto@users.noreply.github.com>
2024-11-09 01:54:09 -0300

    Update fishing_item_card.dart

--- mobile/lib/widgets/fishing_item_card.dart
+++ mobile/lib/widgets/fishing_item_card.dart
@@ -14,11 +14,17 @@ class FishingItemCard extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: ListTile(
         leading: Icon(Icons.sailing, color: Colors.blueAccent, size: 40),
-        title: Text(
-          item.name,
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        title: Text(item.nome, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Peso: ${item.peso} g'),
+            Text('Peso da Isca: ${item.pesoIsca} g'),
+            Text('Material: ${item.material}'),
+            Text('Tipo: ${item.tipo}'),
+            Text('Ambiente: ${item.ambiente}'),
+          ],
         ),
-        subtitle: Text('Peso: ${item.weight} kg'),
       ),
     );
   }
